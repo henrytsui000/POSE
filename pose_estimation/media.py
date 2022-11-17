3b62aba217081151a04626d9b77459208a481853
henrytsui000 <henrytsui000@gmail.com>
2022-11-17 17:05:55 +0800

    :white_check_mark: change all point to specific point

--- pose_estimation/media.py
+++ pose_estimation/media.py
@@ -76,11 +76,16 @@ with mp_pose.Pose(
         # Draw the pose annotation on the image.
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        mp_drawing.draw_landmarks(
-        image,
-        results.pose_landmarks,
-        mp_pose.POSE_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+        # mp_drawing.draw_landmarks(
+        # image,
+        # results.pose_landmarks,
+        # mp_pose.POSE_CONNECTIONS,
+        # landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+        
+        
+        mp_drawing.draw_landmarks(image, PL)
+
+
         # Flip the image horizontally for a selfie-view display.
         cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
         if cv2.waitKey(5) & 0xFF == 27:
