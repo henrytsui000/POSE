cd79e900195d7f97650e818ee654ea291f35e9d9
henrytsui000 <henrytsui000@gmail.com>
2022-11-20 13:14:17 +0800

    :bulb: run.py for start up all system

---
+++ run.py
@@ -0,0 +1,50 @@
+import multiprocessing as mp 
+from threading import Thread
+import logging
+import time
+
+import sys
+# sys.path.insert(1, "./panda")
+
+from panda.Venv import Env
+
+class controller():
+    def __init__(self):
+        logging.basicConfig(level=logging.INFO,
+                            format='%(asctime)s %(levelname)-4s %(message)s',
+                            datefmt='%m-%d %H:%M',)
+        self.env = Env("./src/")
+        self.Plist = [
+            Thread(target = self.penv, args = ()),
+            Thread(target = self.ppe , args = ())
+        ]
+    def run(self):
+        # self.env.run()
+        for p in self.Plist:
+            p.start()
+        for p in self.Plist:
+            p.join()
+        self.env.run()
+
+    def penv(self,):
+        """
+        This is the function which maintain the panda3d environemt        
+        """
+        logging.info("Start running ENV")
+        
+
+    def ppe(self, ):
+        """
+        This is the function which get the human pose estimation
+
+        you don't need give parameters, after run __init__ all the parameters would save in self        
+        """
+        logging.info("Start running PE")
+
+
+def main():
+    env = controller()
+    env.run()
+    
+if __name__ == "__main__":
+    main()
