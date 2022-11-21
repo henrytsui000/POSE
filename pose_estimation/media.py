cbc3354b5651c0958c8b78d52257ab2b8ac8a145
henrytsui000 <henrytsui000@gmail.com>
2022-11-21 15:39:59 +0800

    :bug: let the run.py could run two process

--- pose_estimation/media.py
+++ pose_estimation/media.py
@@ -7,30 +7,24 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
-# For webcam input:
-cap = cv2.VideoCapture(0)
-with mp_pose.Pose(
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            print("Ignoring empty camera frame.")
-            # If loading a video, use 'break' instead of 'continue'.
-            continue
+class Pose():
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+        self.pose = mp_pose.Pose(min_detection_confidence=0.5,
+                                 min_tracking_confidence=0.5)
 
-        # To improve performance, optionally mark the image as not writeable to
-        # pass by reference.
+    def infernce(self, show=False):
+        # while True:
+        _, image = self.cap.read()
         image.flags.writeable = False
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = pose.process(image)
+        results = self.pose.process(image)
         if results.pose_landmarks:
             PL = landmark_pb2.NormalizedLandmarkList(
             landmark = [
                 results.pose_landmarks.landmark[i] for i in range(11, 17)
                 ]
             )
-
         # Draw the pose annotation on the image.
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -38,18 +32,26 @@ with mp_pose.Pose(
         image,
         results.pose_landmarks,
         mp_pose.POSE_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-        # if PL is None:
-        #     continue
-        
-        # mp_drawing.draw_landmarks(image, PL)
+        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())\
 
         image = cv2.flip(image, 1)
-        # cv2.putText(image, "DA", (10, 160), cv2.FONT_HERSHEY_COMPLEX,
-        # 1, (0, 255, 255), 1, cv2.LINE_AA)
+        if show:
+            cv2.imshow('MediaPipe Pose', image)
+        # if cv2.waitKey(5) & 0xFF == 27:
+        #     break
+        cv2.waitKey(5)
 
-        # Flip the image horizontally for a selfie-view display.
-        cv2.imshow('MediaPipe Pose', image)
+    def __del__(self):
+        self.cap.release()
+
+
+
+def main():
+    pose = Pose()
+    while True:
+        pose.infernce(show=True)
         if cv2.waitKey(5) & 0xFF == 27:
             break
-cap.release()
+
+if __name__ == "__main__":
+    main()
