6378f35c754d2df1f1edd348ebe58bab3cd5eb94
henrytsui000 <henrytsui000@gmail.com>
2022-12-04 02:40:15 +0800

    :sparkles: finish rotate function

--- Venv/panda3D.py
+++ Venv/panda3D.py
@@ -5,28 +5,16 @@ from direct.task import Task
 
 import sys
 
-class JointPose():
-    def __init__(self,):
-        self.Joint = {
-
-        }
-    def __getitem__(self,):
-        self.ps = 3
-    def __len__(self):
-        pass
-    def get(self):
-
-
 class Env(ShowBase):
     def __init__(self, src="../src/"):
         super().__init__(self)
         self.accept('escape', sys.exit)
         self.disableMouse()
         self.src = src
-        self.pandaActor = Actor("../src/rp_eric_rigged_001_FBX/rp_eric_rigged_001_ue4.fbx")
+        self.pandaActor = Actor(self.src + "rp_eric_rigged_001_FBX/rp_eric_rigged_001_ue4.fbx")
         self.pandaActor.setScale(0.5, 0.5, 0.5)
         self.pandaActor.setPos(0, 200, -50)
-        self.pandaActor.setHpr(-60, 0, 0)
+        self.pandaActor.setHpr(0, 0, 0)
         self.oriarm = None
 
         tex = Loader.loadTexture(self, self.src + "rp_eric_rigged_001_FBX/tex/rp_eric_rigged_001_dif.jpg")
@@ -34,15 +22,42 @@ class Env(ShowBase):
         self.dir = 1
 
         print(self.pandaActor.listJoints())
-    
+
+        joint_trans = {
+            "UAR": "upperarm_r",
+            "LAR": "lowerarm_r",
+            "UAL": "upperarm_l",
+            "LAL": "lowerarm_l",
+        }
+
+        self.joint_dict = dict()
+        self.rotate_target = dict()
+        self.joint_list = ["UAR", "UAL", "LAR", "LAL"]
+        for joint_name in self.joint_list:
+            self.rotate_target[joint_name] = (60, 30, 90)
+            self.joint_dict[joint_name] = \
+                self.pandaActor.controlJoint(None, "modelRoot", joint_trans[joint_name])
+
         self.node = self.pandaActor.controlJoint(None, "modelRoot", "upperarm_r")
-        self.taskMgr.add(self.rotate_human, "rotate_human")
+        self.taskMgr.add(self.rotate_human_joint, "rotate_human")
         self.pandaActor.reparentTo(self.render)
+        
+        # Debug Function
         self.accept("enter", self.chg)
 
-    def rotate_human(self, task):
+
+    def update_pos_target(self, update_dict):
+        self.joint = update_dict
+
+    def rotate_human_joint(self, task):
+        for joint_name in self.joint_list:
+            self.joint_dict[joint_name].setHpr(*self.rotate_target[joint_name])
+        return Task.cont
+
+    def test_rotate_human_joint(self, task):
         angleDegrees = 1 * self.dir
-        self.node.setHpr(self.node, 0, 0, angleDegrees) 
+        pose = (0, 0, angleDegrees)
+        self.node.setHpr(*pose) 
         return Task.cont
 
     def chg(self):
