a5c4b351a854bb65bee3ea9a485ef5aa7779139a
henrytsui000 <henrytsui000@gmail.com>
2022-12-03 21:03:23 +0800

    :bug: make human joint rotate correctly

--- Venv/panda3D.py
+++ Venv/panda3D.py
@@ -5,28 +5,37 @@ from direct.task import Task
 
 import sys
 
+class JointPose():
+    def __init__(self,):
+        self.Joint = {
+
+        }
+    def __getitem__(self,):
+        self.ps = 3
+    def __len__(self):
+        pass
+    def get(self):
+
+
 class Env(ShowBase):
     def __init__(self, src="../src/"):
         super().__init__(self)
         self.accept('escape', sys.exit)
         self.disableMouse()
         self.src = src
-
-        self.pandaActor = Actor(self.src + 'human_model/human.fbx')
-        # self.pandaActor = Actor(self.src + 'human_model/robot')
-        # self.pandaActor = Actor(self.src + 'city-models.obj/lpFemale_casual_B-model.obj')
-        self.pandaActor.setScale(10, 10, 10)
+        self.pandaActor = Actor("../src/rp_eric_rigged_001_FBX/rp_eric_rigged_001_ue4.fbx")
+        self.pandaActor.setScale(0.5, 0.5, 0.5)
         self.pandaActor.setPos(0, 200, -50)
         self.pandaActor.setHpr(-60, 0, 0)
         self.oriarm = None
 
-        tex = Loader.loadTexture(self, self.src + "texture/world_people_colors.png")
+        tex = Loader.loadTexture(self, self.src + "rp_eric_rigged_001_FBX/tex/rp_eric_rigged_001_dif.jpg")
         self.pandaActor.setTexture(tex, 1)
         self.dir = 1
 
         print(self.pandaActor.listJoints())
     
-        self.node = self.pandaActor.controlJoint(None, "modelRoot", "joint9")
+        self.node = self.pandaActor.controlJoint(None, "modelRoot", "upperarm_r")
         self.taskMgr.add(self.rotate_human, "rotate_human")
         self.pandaActor.reparentTo(self.render)
         self.accept("enter", self.chg)
