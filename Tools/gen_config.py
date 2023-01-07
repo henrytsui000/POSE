b5df34f30718912e439047c29d13129896ea382f
LucyTuan <yztuan1129@gmail.com>
2023-01-07 16:22:55 +0800

    :wrench: Update contents of config. files

--- Tools/gen_config.py
+++ Tools/gen_config.py
@@ -13,18 +13,18 @@ dic = {
                 "LH" : ("L_sho", "L_wri", "R_sho", "L_sho"),
                 "RH" : ("R_sho", "R_wri", "L_sho", "R_sho"),
                 "LH_U" : ("L_sho", "L_elb", "R_sho", "L_sho"),
-                "LH_D" : ("L_elb", "L_wri", "L_sho", "L_elb"),
+                "LH_D" : ("L_elb", "L_wri", "R_sho", "L_sho"),
                 "RH_U" : ("R_sho", "R_elb","L_sho", "R_sho"),
-                "RH_D" : ("R_elb", "R_wri","R_sho", "R_elb"),
+                "RH_D" : ("R_elb", "R_wri","L_sho", "R_sho"),
                 # Hip
                 "HIP":("L_hip","R_hip","L_hip","R_hip"),
                 # Feet
                 "LF" : ("L_hip", "L_ank", "R_hip", "L_hip"),
                 "RF" : ("R_hip", "R_ank", "L_hip", "R_hip"),
                 "LF_U" : ("L_hip", "L_knee", "R_hip", "L_hip"),
-                "LF_D" : ("L_knee", "L_ank", "L_hip", "L_knee"),
+                "LF_D" : ("L_knee", "L_ank", "R_hip", "L_hip"),
                 "RF_U" : ("R_hip", "R_knee","L_hip", "R_hip"),
-                "RF_D" : ("R_hip", "R_ank","R_hip", "R_knee"),  }     
+                "RF_D" : ("R_hip", "R_ank","L_hip", "R_hip"),  }     
 
              
 }
